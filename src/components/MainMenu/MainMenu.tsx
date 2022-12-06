9bcdc91c5b1897bd882a46584edd3e90e72a941a
nikolagajovic <iamnikolagajovic@gmail.com>
2022-12-06 13:55:23 +0100

    added UserRegistratioPage

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Button } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import './MainMenu.css';
 import {HashRouter, Link } from 'react-router-dom';
 
@@ -26,7 +26,7 @@ interface MainMenuState{
 export class MainMenu extends React.Component<MainMenuProperties> {
     state: MainMenuState;
 
-    
+   
 
     constructor(props: Readonly<MainMenuProperties>) {
         super(props);
@@ -38,47 +38,57 @@ export class MainMenu extends React.Component<MainMenuProperties> {
 
     // novo setovanje itema
 
+    
     setItems( items: MainMenuItem[] ){
         this.setState({
             items:items,
         })
     }
 
+    
+
     render() {
         return(
-                
-       
-
+            
+            
           <Nav variant="tabs">
             <HashRouter>
            {this.state.items.map(this.makeNavLink)}
            </HashRouter>
           </Nav>
                
-            
+          
        
         );
     }
+
+   
+
+    
     
 
     private makeNavLink(item: MainMenuItem){
+        
         return(
             	 
             
-          
+            
 
             <div className='buttons'>
              <Link to= {item.link} className="nav-link">
                 {item.text}
              </Link>
+             
              </div>
-        
+             
             
             
         )
+
+       
     }
  
-
-    
+   
+ 
 
 }
