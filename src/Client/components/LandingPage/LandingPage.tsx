b88bdea7484e1ca2c0c9ee612df8a1aa4a9a6b25
nikolagajovic <iamnikolagajovic@gmail.com>
2022-12-07 14:51:56 +0100

    added card on landing page

--- src/Client/components/LandingPage/LandingPage.tsx
+++ src/Client/components/LandingPage/LandingPage.tsx
@@ -1,4 +1,6 @@
+import { relative } from 'path';
 import React from 'react';
+import { Card, Container } from 'react-bootstrap';
 import { MainMenu, MainMenuItem } from '../MainMenu/MainMenu';
 
 const menuItems = [
@@ -8,8 +10,118 @@ const menuItems = [
 
 function LandingPage() {
   return (
-      <MainMenu items = {menuItems}></MainMenu>
-  ); 
-}
+      <><MainMenu items={menuItems}></MainMenu>
+      
+        <Container style = {{ marginTop:  '50px' }}>
+       
+  
+    <Card style={{ width: 'auto', height:'auto' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd Some quick example text to build on the card title and make up the
+          bulk of the card's content.askohjajskhldhiaspdoasodhopiash dapsiashdosh
+          asdaosuidgauisdajksbdaiusgdoaisdioagsd
+          askoidhasidgasoidgasihdaisgd
+          aspodhasoidhasihdasihdaishhoiasd
+          asdčophasidohasihdaishdiohasd
+
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  
+
+      </Container>
+      
+      
+      
+      
+      </>
+        
+      );
+    }
+
+
 
 export default LandingPage;
