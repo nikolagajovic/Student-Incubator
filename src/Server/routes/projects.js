4cfdb8ffba2738c296d536bf3b08ce6fcd2ec237
kojan00 <pericjankokg@gmail.com>
2022-12-08 13:26:55 +0100

    added projects api

---
+++ src/Server/routes/projects.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../config/dbconnection")
+const queries = require("../queries/queries")
+
+router.get("/projects", async (req, res) => {
+    try {
+        const results = await pool.query("SELECT * FROM projectinfo");
+        const response = results.rows;
+        res.json(response);
+        
+        
+    } catch (er) {
+        console.error(er.message);
+        res.status(500).json("Server error")
+    }
+});
+
+module.exports = router;
