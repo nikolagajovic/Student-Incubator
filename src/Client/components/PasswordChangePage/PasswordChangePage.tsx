dd7e8b732aabe400b6e742fa2c96a05dbad9aa05
Tmizzle <aleksandarplanic21@gmail.com>
2022-12-12 09:49:09 +0100

    SendData in PasswordChange works with real params

--- src/Client/components/PasswordChangePage/PasswordChangePage.tsx
+++ src/Client/components/PasswordChangePage/PasswordChangePage.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PasswordChangePage.css'
 import { Button, Card, Container, Form } from 'react-bootstrap';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
 
-
-const SendData = () => {
-const reactData = [localStorage.getItem("token")];
-const url = "http://localhost:5000/home/change-password";
-
-    axios.put( url , {
-        currentPassword: 'user123',
-        newPassword: 'user1234'
-      })
-      .then((response) => {
-        console.log(response);
-      }, (error) => {
-        console.log(error);
-      });
-}
-
-export default class UserRegistrationPage extends React.Component{
-    render() {
+export default function UserPasswordChangePage() {
+        const reactData = [localStorage.getItem("token")];
+        const url = "http://localhost:5000/home/change-password";
+        const [newPassword, SetNewPassword] = useState("");
+        const [newRepeatPassword, SetRepeatNewPassword] = useState("");
+        const [currentPassword, SetOldPassword] = useState("");
+        
+        const SendData = () => {
+            fetch( url , {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    currentPassword: currentPassword,
+                    newPassword: newPassword,
+                    token: reactData
+                })
+            }).then((response) => {
+                if(response.status === 200){
+                    alert("Success")
+                }
+              }, (error) => {
+                console.log(error);
+              });
+        }
         return(
             <Container>
                 <Card className="passwordchangeCard">
@@ -35,15 +42,21 @@ export default class UserRegistrationPage extends React.Component{
                             <Form className="passwordchangeForm">
                                 <Form.Group className="passwordchangetextForm">
                                     <Form.Label ClassName="textField" htmlFOr="password">New Password:</Form.Label>
-                                    <Form.Control type="password" id="password"></Form.Control>
+                                    <Form.Control type="password" id="Newpassword"
+                                    value={ newPassword }
+                                    onChange= { event => SetNewPassword(event.target.value) }></Form.Control>
                                 </Form.Group>
                                 <Form.Group className="passwordchangetextForm">
                                     <Form.Label ClassName="textField" htmlFOr="password">Repeat new password:</Form.Label>
-                                    <Form.Control type="password" id="password"></Form.Control>
+                                    <Form.Control type="password" id="RepeatNewpassword"
+                                    value={ newRepeatPassword }
+                                    onChange= { event => SetRepeatNewPassword(event.target.value) }></Form.Control>
                                 </Form.Group>
                                 <Form.Group className="passwordchangetextForm">
                                     <Form.Label ClassName="textField" htmlFOr="password">Old Password:</Form.Label>
-                                    <Form.Control type="password" id="password"></Form.Control>
+                                    <Form.Control type="password" id="Oldpassword"
+                                    value={ currentPassword }
+                                    onChange= { event => SetOldPassword(event.target.value) }></Form.Control>
                                 </Form.Group>
                                 <Form.Group className="passwordchangebuttonForm">
                                     <Button className="btn" variant="primary"
@@ -55,5 +68,4 @@ export default class UserRegistrationPage extends React.Component{
                 </Card>
             </Container>
         )
-    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LandingPage from './Client/components/LandingPage/LandingPage';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.js'
 import 'popper.js/dist/popper.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { HashRouter, Switch, Route } from 'react-router-dom';
-import ProjectsPage from './Client/components/ProjectsPage/ProjectsPage';
-import { MyProjectsPage } from './Client/components/MyProjectsPage/MyProjectsPage';
-import { MyProfilePage } from './Client/components/MyProfilePage/MyProfilePage';
-import UserLoginPage from './Client/components/UserLoginPage/UserLoginPage';
-import { UserRegistrationPage } from './Client/components/UserRegistrationPage/UserRegistrationPage';
-import { HomePage } from './Client/components/HomePage/HomePage';
-import PasswordChangePage from './Client/components/PasswordChangePage/PasswordChangePage';
 import { TokenProvider } from './Client/context/TokenContext/TokenProvider';
 import Router from './Client/components/Router/Router';
 
