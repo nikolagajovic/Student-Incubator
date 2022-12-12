9093001d010e9b8d1836711ddbf1f7606b4a07d1
kojan00 <pericjankokg@gmail.com>
2022-12-12 13:38:13 +0100

    password change works with decoded token now

--- src/Server/queries/queries.js
+++ src/Server/queries/queries.js
@@ -1,5 +1,7 @@
 const getAllProjects = "SELECT * FROM projectinfo";
+const getAccInfoById = "SELECT * FROM accountinfo WHERE id = $1";
 
 module.exports = {
     getAllProjects,
+    getAccInfoById,
 }
